4ea5354a22eb1de3102d82699072f8293c1094fa
Emmanuel Mendieta <emmanuel_mendieta@icloud.com>
2025-01-25 03:01:50 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', // Enable static export
-  basePath: '/lever-web', // Specify the repository name
-  assetPrefix: '/lever-web/', // Set the prefix for assets
+
 };
 
 export default nextConfig;
