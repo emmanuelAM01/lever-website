c1f5bbfc3593adadc81cb791a81bdb9147ba905c
Emmanuel <emman0621@gmail.com>
2025-03-26 23:25:29 +0400

    fix: copyright date

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="w-full text-center py-6 px-4 sm:px-10 lg:px-32 text-textPrimary/60 text-md">
-        Lever Labs, Inc.
+        © 2025 Lever Labs, Inc.
       </footer>
     );
   };
